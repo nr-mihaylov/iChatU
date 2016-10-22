1f039cabbeddea4d2ce31854bd5da74851ec7df3
Nikolay Rumenov Mihaylov <nr.mihaylov@gmail.com>
2016-10-22 19:04:46 +0200

    Reorganize project structure

--- client/js/client.js
+++ client/js/client.js
@@ -49,7 +49,7 @@ var SocketClient = function() {
 			if(socket && socket.connected) 
 				return socket.emit('authentication', packet);
 
-			socket = io.connect('localhost:3000');
+			socket = io.connect('188.166.160.74:3000');
 
 			this.session = socket.session = socket.session || {};
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,65 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var minifycss = require('gulp-clean-css');
+var minifyjs = require('gulp-minify');
+var rename = require('gulp-rename');
 var nodemon = require('gulp-nodemon');
 
-gulp.task('default', ['nodemon', 'sass-watch']);
+gulp.task('default', [
+	'nodemon', 
+	'sass', 
+	'js', 
+	'sass-watch', 
+	'js-watch'
+]);
+
+gulp.task('compile', [ 
+	'sass', 
+	'js'
+]);
 
 gulp.task('sass-watch', function () {
-	gulp.watch('./scss/**/*.scss', ['sass']);
+	gulp.watch('./client/scss/*.scss', ['sass']);
+});
+
+gulp.task('js-watch', function () {
+	gulp.watch('./client/js/*.js', ['js']);
 });
 
 gulp.task('sass', function() {
 	
-	return gulp.src('./scss/**/*.scss')
+	return gulp.src('./client/scss/style.scss')
 		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('./public/stylesheets/'));
+		.pipe(minifycss())
+		.pipe(rename({
+			basename: "styles",
+			suffix: ".min",
+			extname: ".css"	
+		}))
+		.pipe(gulp.dest('./public/stylesheets/'))
+		.pipe(gulp.dest('./docs/stylesheets/'));
 
+});
+
+gulp.task('js', function() {
+	return gulp.src([
+		'./client/js/util.js', 
+		'./client/js/ui.js', 
+		'./client/js/shell.js', 
+		'./client/js/client.js', 
+		'./client/js/commandHandler.js', 
+		'./client/js/ichatu.js'
+	])
+	.pipe(concat('script.js'))
+	.pipe(minifyjs({
+			ext: {
+				min: '.min.js'
+			},
+			noSource: true
+		}))
+	.pipe(gulp.dest('./public/javascripts/'))
+	.pipe(gulp.dest('./docs/javascripts/'));
 });
 
 gulp.task('nodemon', function () {
